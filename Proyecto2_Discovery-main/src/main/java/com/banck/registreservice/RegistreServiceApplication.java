20131d43b82489f0a21de9e567fc19e996278a09
joseniquen <62155219+joseniquen@users.noreply.github.com>
2021-12-06 10:42:49 -0500

    f
    d

--- Proyecto2_Discovery-main/src/main/java/com/banck/registreservice/RegistreServiceApplication.java
+++ Proyecto2_Discovery-main/src/main/java/com/banck/registreservice/RegistreServiceApplication.java
@@ -3,7 +3,6 @@ package com.banck.registreservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-
 @EnableEurekaServer
 @SpringBootApplication
 public class RegistreServiceApplication {
